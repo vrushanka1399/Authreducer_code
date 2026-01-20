468fa138e20c80c94c6e6368470c3e7be5185477
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2026-01-20 16:32:57 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,86 @@
 ﻿import { useDispatch, useSelector } from "react-redux";
-import { toggleCart } from "./cartSlice";
+import { addToCart, removeFromCart } from "./cartSlice";
+
+const DUMMY_PRODUCTS = [
+  { id: "p1", title: "iPhone", price: 80000 },
+  { id: "p2", title: "Laptop", price: 60000 },
+  { id: "p3", title: "Headphones", price: 2000 }
+];
 
 function App() {
 
   const dispatch = useDispatch();
-  const isCartVisible = useSelector(
-    state => state.cart.isVisible
-  );
+  const cart = useSelector(state => state.cart);
 
   return (
-    <div style={{ padding: "30px" }}>
-      <button
-        onClick={() => dispatch(toggleCart())}
-      >
-        MyCart
-      </button>
+    <div style={{ padding: 30 }}>
 
-      {isCartVisible && (
-        <div style={{
-          marginTop: "20px",
-          padding: "20px",
-          border: "1px solid black"
-        }}>
-          <h3>🛒 My Cart</h3>
-          <p>Your cart items will appear here</p>
+      {/* CART ICON */}
+      <h2>
+        🛒 My Cart ({cart.totalQuantity})
+      </h2>
+
+      <hr />
+
+      {/* PRODUCTS */}
+      <h3>Products</h3>
+
+      {DUMMY_PRODUCTS.map(prod => (
+        <div key={prod.id} style={{ marginBottom: 10 }}>
+          <b>{prod.title}</b> - ₹{prod.price}
+
+          <button
+            style={{ marginLeft: 10 }}
+            onClick={() =>
+              dispatch(addToCart(prod))
+            }
+          >
+            Add To Cart
+          </button>
         </div>
+      ))}
+
+      <hr />
+
+      {/* CART ITEMS */}
+      <h3>Cart Items</h3>
+
+      {cart.items.length === 0 && (
+        <p>Cart is empty</p>
       )}
+
+      {cart.items.map(item => (
+        <div
+          key={item.id}
+          style={{
+            border: "1px solid black",
+            marginBottom: 10,
+            padding: 10
+          }}
+        >
+          <h4>{item.title}</h4>
+          <p>Price: ₹{item.price}</p>
+          <p>Quantity: {item.quantity}</p>
+
+          <button
+            onClick={() =>
+              dispatch(addToCart(item))
+            }
+          >
+            +
+          </button>
+
+          <button
+            onClick={() =>
+              dispatch(removeFromCart(item.id))
+            }
+            style={{ marginLeft: 10 }}
+          >
+            -
+          </button>
+        </div>
+      ))}
+
     </div>
   );
 }
--- cartSlice.js
+++ cartSlice.js
@@ -3,14 +3,47 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    isVisible: false
+    items: [],   // {id, title, price, quantity}
+    totalQuantity: 0
   },
   reducers: {
-    toggleCart(state) {
-      state.isVisible = !state.isVisible;
+
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        item => item.id === newItem.id
+      );
+
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.items.push({
+          ...newItem,
+          quantity: 1
+        });
+      } else {
+        existingItem.quantity++;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(
+        item => item.id === id
+      );
+
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(
+          item => item.id !== id
+        );
+      } else {
+        existingItem.quantity--;
+      }
     }
   }
 });
 
-export const { toggleCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
